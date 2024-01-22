59e48c55adcdf5a1df9c2fcf98fcd4b3a0875b95
sivakorn28-7 <645051200287@mail.rmutk.ac.th>
2024-01-23 01:50:05 +0700

    finish

--- app/src/main/java/com/example/ez_calculator/MainActivity.kt
+++ app/src/main/java/com/example/ez_calculator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ez_calculator
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,6 +11,7 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SuspiciousIndentation", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -38,16 +40,18 @@ class MainActivity : AppCompatActivity() {
 
         val Tans: TextView = findViewById(R.id.Tans)
 
-        var currentNumber = ""
+        var currentNumber = "0"
         var Number1 = ""
         var Number2 = ""
         var currentOperand = ""
+        var dot:Boolean = false
 
 
         Btclear.setOnClickListener {
-            currentNumber = ""
-            Tans.setText("0")
+            currentNumber = "0"
+            Tans.text = currentNumber
             currentOperand = ""
+            dot = false
 
         }
 
@@ -79,6 +83,7 @@ class MainActivity : AppCompatActivity() {
             Number1 = currentNumber
             currentNumber = "0"
             Tans.text = currentNumber
+            dot = false
 //            currentNumber = ""
 //            Tans.text = "0"
         }
@@ -88,6 +93,7 @@ class MainActivity : AppCompatActivity() {
             Number1 = currentNumber
             currentNumber = "0"
             Tans.text = currentNumber
+            dot = false
         }
 
         Btmulti.setOnClickListener {
@@ -95,6 +101,7 @@ class MainActivity : AppCompatActivity() {
             Number1 = currentNumber
             currentNumber = "0"
             Tans.text = currentNumber
+            dot = false
         }
 
         Btdivide.setOnClickListener {
@@ -102,6 +109,7 @@ class MainActivity : AppCompatActivity() {
             Number1 = currentNumber
             currentNumber = "0"
             Tans.text = currentNumber
+            dot = false
         }
 
         Btmod.setOnClickListener {
@@ -109,7 +117,7 @@ class MainActivity : AppCompatActivity() {
             Number1 = currentNumber
             currentNumber = "0"
             Tans.text = currentNumber
-
+            dot = false
         }
 
         Btequal.setOnClickListener {
@@ -117,27 +125,27 @@ class MainActivity : AppCompatActivity() {
             var result = 0.0
 
                 when (currentOperand) {
-                    "+" -> result = Number1.toDouble()   + Number2.toDouble()
+                    "+" -> result = Number1.toDouble() + Number2.toDouble()
                     "-" -> result = Number1.toDouble() - Number2.toDouble()
                     "*" -> result = Number1.toDouble() * Number2.toDouble()
-                    "/" -> {
-                        if (Number2.toDouble() == 0.0) {
-                            Tans.text = "Error: Division by zero"
-                        } else {
-                            result = Number1.toDouble() / Number2.toDouble()
-                        }
-                    }
+                    "/" -> result = Number1.toDouble() / Number2.toDouble()
                     "%" -> result =  Number1.toDouble() % Number2.toDouble()
                 }
-                Tans.text = result.toString() // Display the result
 
+                if(result.toString() == "Infinity" ) {
+                    Tans.text = "Error: Division by zero"
+                }else {
+                    Tans.text = result.toString() // Display the result
+                }
+            Number1 = result.toString()
+            currentNumber = result.toString()
         }
 
         Bt0.setOnClickListener {
             //currentNumber = Tans.text.toString()
             if (currentNumber == "0") {
                 currentNumber = "0"
-                Tans.setText("0")
+                Tans.text = "0"
             } else {
                 currentNumber += "0"
                 Tans.text = currentNumber
@@ -154,7 +162,7 @@ class MainActivity : AppCompatActivity() {
         Bt1.setOnClickListener {
             if(currentNumber == "0"){
                 currentNumber = "1"
-                Tans.setText("1")
+                Tans.text = "1"
             }else{
                 currentNumber += "1"
                 Tans.text = currentNumber
@@ -165,7 +173,7 @@ class MainActivity : AppCompatActivity() {
         Bt2.setOnClickListener {
             if(currentNumber == "0"){
                 currentNumber = "2"
-                Tans.setText("2")
+                Tans.text = "2"
             }else{
                 currentNumber += "2"
                 Tans.text = currentNumber
@@ -175,7 +183,7 @@ class MainActivity : AppCompatActivity() {
         Bt3.setOnClickListener {
             if(currentNumber == "0"){
                 currentNumber = "3"
-                Tans.setText("3")
+                Tans.text = "3"
             }else{
                 currentNumber += "3"
                 Tans.text = currentNumber
@@ -185,7 +193,7 @@ class MainActivity : AppCompatActivity() {
         Bt4.setOnClickListener {
             if(currentNumber == "0"){
                 currentNumber = "4"
-                Tans.setText("4")
+                Tans.text = "4"
             }else{
                 currentNumber += "4"
                 Tans.text = currentNumber
@@ -195,7 +203,7 @@ class MainActivity : AppCompatActivity() {
         Bt5.setOnClickListener {
             if(currentNumber == "0"){
                 currentNumber = "5"
-                Tans.setText("5")
+                Tans.text = "5"
             }else{
                 currentNumber += "5"
                 Tans.text = currentNumber
@@ -205,7 +213,7 @@ class MainActivity : AppCompatActivity() {
         Bt6.setOnClickListener {
             if(currentNumber == "0"){
                 currentNumber = "6"
-                Tans.setText("6")
+                Tans.text = "6"
             }else{
                 currentNumber += "6"
                 Tans.text = currentNumber
@@ -215,7 +223,7 @@ class MainActivity : AppCompatActivity() {
         Bt7.setOnClickListener {
             if(currentNumber == "0"){
                 currentNumber = "7"
-                Tans.setText("7")
+                Tans.text = "7"
             }else{
                 currentNumber += "7"
                 Tans.text = currentNumber
@@ -225,7 +233,7 @@ class MainActivity : AppCompatActivity() {
         Bt8.setOnClickListener {
             if(currentNumber == "0"){
                 currentNumber = "8"
-                Tans.setText("8")
+                Tans.text = "8"
             }else{
                 currentNumber += "8"
                 Tans.text = currentNumber
@@ -235,141 +243,20 @@ class MainActivity : AppCompatActivity() {
         Bt9.setOnClickListener {
             if(currentNumber == "0"){
                 currentNumber = "9"
-                Tans.setText("9")
+                Tans.text = "9"
             }else{
                 currentNumber += "9"
                 Tans.text = currentNumber
             }
         }
+
+        Btdot.setOnClickListener {
+            if (dot == false) {
+                currentNumber += "."
+                Tans.text = currentNumber
+                dot = true
+            }
+        }
     }
 }
 
-//package com.example.calculator
-//
-//import android.os.Bundle
-//import android.util.Log
-//import android.view.View
-//import android.widget.Button
-//import android.widget.TextView
-//import androidx.appcompat.app.AppCompatActivity
-//
-//class MainActivity : AppCompatActivity() {
-//
-//    private lateinit var resultTextView: TextView
-//    private var currentInput: String = ""
-//    private var currentOperator: String? = ""
-//    private var firstOperand: Double? = 0.0
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//
-//        resultTextView = findViewById(R.id.textView)
-//
-//        // เพิ่ม OnClickListener ให้กับปุ่มทุกปุ่มตัวเลข
-//        val numberButtons = arrayOf(
-//            R.id.button5, R.id.button6, R.id.button7, R.id.button9,
-//            R.id.button10, R.id.button11, R.id.button13, R.id.button14,
-//            R.id.button15, R.id.button17
-//        )
-//
-//        for (buttonId in numberButtons) {
-//            findViewById<Button>(buttonId).setOnClickListener {
-//                onNumberButtonClick(it)
-//            }
-//        }
-//
-//        // เพิ่ม OnClickListener ให้กับปุ่มลบ
-//        findViewById<Button>(R.id.button2).setOnClickListener {
-//            onDeleteButtonClick()
-//        }
-//
-//        // เพิ่ม OnClickListener ให้กับปุ่มลบทั้งหมด
-//        findViewById<Button>(R.id.button1).setOnClickListener {
-//            clearAll()
-//        }
-//
-//
-//        // เพิ่ม OnClickListener ให้กับปุ่มเพิ่ม, ลบ, คูณ, หาร, modulo
-//        val operatorButtons = arrayOf(
-//            R.id.button16, R.id.button12, R.id.button8,R.id.button4, R.id.button3
-//        )
-//
-//        for (buttonId in operatorButtons) {
-//            findViewById<Button>(buttonId).setOnClickListener {
-//                onOperatorButtonClick(it)
-//            }
-//        }
-//
-//        // เพิ่ม OnClickListener ให้กับปุ่มเท่ากับ
-//        findViewById<Button>(R.id.button19).setOnClickListener {
-//            onEqualsButtonClick()
-//        }
-//
-//    }
-//
-//    private fun clearAll() {
-//        currentInput = "0"
-//        currentOperator = null
-//        firstOperand = null
-//        updateResult()
-//    }
-//
-//
-//    private fun onNumberButtonClick(view: View) {
-//        val buttonText = (view as Button).text.toString()
-//
-//        if (currentInput == "0") {
-//            // กรณีที่เป็นเลข 0 เลขเดียวบนหน้าจอ ไม่เพิ่มจำนวนเลข 0
-//            currentInput = buttonText
-//        } else {
-//            currentInput += buttonText
-//        }
-//
-//        updateResult()
-//    }
-//
-//    private fun onDeleteButtonClick() {
-//        if (currentInput.length > 1) {
-//            // ลบตัวเลขที่พิมพ์ล่าสุดทีละ 1 หลัก
-//            currentInput = currentInput.substring(0, currentInput.length - 1)
-//        } else {
-//            // หากเหลือเลขหลักเดียว กดลบซ้ำเป็นเลข 0
-//            currentInput = "0"
-//        }
-//
-//        updateResult()
-//    }
-//
-//    private fun onOperatorButtonClick(view: View) {
-//        currentOperator = (view as Button).text.toString()
-//        firstOperand = currentInput.toDouble()
-//        currentInput = "0"
-//    }
-//
-//    private fun onEqualsButtonClick() {
-//        if (currentOperator != null && firstOperand != null) {
-//            val secondOperand = currentInput.toDouble()
-//            val result = when (currentOperator) {
-//                "+" -> firstOperand!! + secondOperand
-//                "-" -> firstOperand!! - secondOperand
-//                "x" -> firstOperand!! * secondOperand
-//                "/" -> firstOperand!! / secondOperand
-//                "%" -> firstOperand!! % secondOperand
-//                else -> throw IllegalArgumentException("Invalid operator")
-//            }
-//
-//            currentInput = result.toString()
-//            currentOperator = null
-//            firstOperand = null
-//
-//            updateResult()
-//        }
-//    }
-//
-//
-//
-//    private fun updateResult() {
-//        resultTextView.text = currentInput
-//    }
-//}
